fbfcf096f213ac8d1aca962c905ba37e2b5c0cdd
faizakmalz <faztcollege@gmail.com>
2024-02-20 13:10:02 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// const dropdown2 = document.getElementById('dropdown2')
+// const productsButton = document.getElementById('productsButton')
+
+// productsButton.addEventListener('click', function() {
+//     dropdown2.removeAttribute('hidden')
+//     this.dispatchEvent
+// })
+
+function toggleDropdown(dropdownID) {
+    const dropdown = document.getElementById(dropdownID)
+    dropdown.classList.toggle('hidden');
+}
+
+
+
+
+
